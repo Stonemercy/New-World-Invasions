1aeca20844af4b2771db920520bdfb09c7ce43a3
Lee <lee_pritchard@rocketmail.com>
2023-09-26 13:25:23 +0100

    0.11
    - removed use of regex
    - commented a lot
    - added confirmation of reports
    - added autocomplete
    
    Todo:
    - fix the confirmation text to include is_important
    - maybe embeds for responses?

--- cogs/invasions.py
+++ cogs/invasions.py
@@ -1,18 +1,19 @@
 import disnake
 import lists
 from disnake.ext import commands, tasks
-import re
 import sqlite3
 import datetime
 
-# connect to db
+# connect to db when cog is loaded - shouldn't close while cog is functional
 con = sqlite3.connect("invasions.db")
 cur = con.cursor()
 actives = cur.execute("SELECT * FROM invasions")
 activeInvasions = actives.fetchall()
 
-resetTime = datetime.time(hour=14, minute=38)
+# the time the database resets (if bot is running)
+resetTime = datetime.time(hour=23, minute=00)
 
+# the task that reset the db at time of reset (above)
 @tasks.loop(time=resetTime)
 async def cleardb():
     cur.execute("DELETE FROM invasions")
@@ -20,24 +21,41 @@ async def cleardb():
     print("Cleared the database")
     con.close()
 
-# Regex to check time
-def time_regex(input_text):
-    pattern = re.compile(r"\d\d:\d\d")
-    return pattern.match(input_text)
-
-# Lists of zones and times
+# lists of zones and times
+# also converted to a csv string for when someone puts the wrong things in
 zoneList = lists.zones
 timeList = lists.times
+yes_no = ['yes', 'no']
 zoneStrings = ', '.join(zoneList)
+timeStrings = ', '.join(timeList)
 
+#autocomplete languages
+async def zone_autocomplete(inter: disnake.ApplicationCommandInteraction, user_input: str):
+    if not str:
+        return ["Type in a zone"]
+    return [zones for zones in zoneList if user_input.upper() in zones]
+async def time_autocomplete(inter: disnake.ApplicationCommandInteraction, user_input: str):
+    if not str:
+        return ["Type in a time"]
+    return [time for time in timeList if user_input in time]
+async def yesno_autocomplete(inter: disnake.ApplicationCommandInteraction, user_input: str):
+    if not str:
+        return ["yes/no"]
+    return [yesno for yesno in yes_no if user_input in yesno]
+
+user_inputs = []
+
+# the entire cog for the invasions command
 class InvasionsCommand(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
     
+    #initial invasions command group
     @commands.slash_command()
     async def invasions(self, inter: disnake.ApplicationCommandInteraction):
         pass
     
+    # invasions list subcommand
     @invasions.sub_command(description="List active (and reported) invasions")
     async def list(self, inter: disnake.ApplicationCommandInteraction):
         if activeInvasions == []:
@@ -53,21 +71,44 @@ class InvasionsCommand(commands.Cog):
             await inter.response.send_message(f"Invasions for today:\n{str(invasion1[0])} - {str(invasion1[1])} - Important:{str(invasion1[2])}\n{str(invasion2[0])} - {str(invasion2[1])} - Important: {str(invasion2[2])}\n{str(invasion3[0])} - {str(invasion3[1])} - Important: {str(invasion3[2])}") 
         elif len(activeInvasions) == 4:
             invasion1, invasion2, invasion3, invasion4 = activeInvasions[0], activeInvasions[1], activeInvasions[2], activeInvasions[3]
-            await inter.response.send_message(f"Invasions for today:\n{str(invasion1[0])} - {str(invasion1[1])} - Important: {str(invasion1[2])}\n{str(invasion2[0])} - {str(invasion2[1])} - Important: {str(invasion2[2])}\n{str(invasion3[0])} - {str(invasion3[1])} - Important: {str(invasion3[2])}\n{str(invasion4[0])} - {str(invasion4[1])} - Important: {str(invasion4[2])}") 
-        con.close()
+            await inter.response.send_message(f"Invasions for today:\n{str(invasion1[0])} - {str(invasion1[1])} - Important: {str(invasion1[2])}\n{str(invasion2[0])} - {str(invasion2[1])} - Important: {str(invasion2[2])}\n{str(invasion3[0])} - {str(invasion3[1])} - Important: {str(invasion3[2])}\n{str(invasion4[0])} - {str(invasion4[1])} - Important: {str(invasion4[2])}")
     
+    # invasions report subcommand
     @invasions.sub_command(description="Report an invasion")
-    async def report(self, inter: disnake.ApplicationCommandInteraction, zone:str, time:str):
-        if zone.capitalize() not in zoneList:
+    async def report(self,
+        inter: disnake.ApplicationCommandInteraction,
+        zone:str = commands.param(autocomplete=zone_autocomplete),
+        time:str = commands.param(autocomplete=time_autocomplete),
+        is_important:str = commands.param(autocomplete=yesno_autocomplete)):
+
+        # changes the global to the inputs
+        global user_inputs
+        user_inputs = [zone.capitalize(), time, is_important]
+
+        if zone not in zoneList:
             await inter.response.send_message(f"Your zone is not in the list of zones, pleases select one of the following:\n{zoneStrings}")
-        elif time_regex(time) == None:
-            await inter.response.send_message("Time needs to be formatted like so: 18:00")
+        elif time not in timeList:
+            await inter.response.send_message(f"Please choose one of the following times:\n{timeStrings}")
         else:
-            data = (zone, time)
-            cur.execute("INSERT INTO invasions VALUES (?, ?, 'No')", data)
+            await inter.response.send_message(f"You have provided the invasion of {zone.capitalize()} at {time}, is this correct?", components=[
+                disnake.ui.Button(label="Yes", style=disnake.ButtonStyle.success, custom_id="yes1"),
+                disnake.ui.Button(label="No", style=disnake.ButtonStyle.danger, custom_id="no1")
+            ])
+    
+    # listener for report confirmation buttons
+    @commands.Cog.listener("on_button_click")
+    async def report_confirmation_listener(self, inter:disnake.MessageInteraction):
+        zone = user_inputs[0]
+        time = user_inputs[1]
+        is_important = user_inputs[2]
+
+        if inter.component.custom_id not in ["yes1", "no1"]:
+            return
+        if inter.component.custom_id == "yes1":
+            print(f"Here's the data: {user_inputs}")
+            cur.execute("INSERT INTO invasions VALUES (?, ?, ?)", user_inputs)
             con.commit()
-            await inter.response.send_message(f"You have provided the invasion of {zone.capitalize()} at {time}")
-        con.close()
+            await inter.response.send_message(f"You have reported the following:\n{zone} at {time} - Important: {is_important.capitalize()}")
 
 def setup(bot: commands.Bot):
     bot.add_cog(InvasionsCommand(bot))
